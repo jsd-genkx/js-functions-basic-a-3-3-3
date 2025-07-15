870a645415fba1c218529d649016e157a29aea90
Your Name <you@example.com>
2025-07-15 16:43:21 +0700

    task02 solution

--- task02.js
+++ task02.js
@@ -14,6 +14,9 @@ Task 02: Check Even or Odd
 */
 
 // 👨‍💻 TODO-01: Write your code below to complete the task as described above.
+function checkEvenOrOdd(num) {
+	return num%2 == 0 ? "Even" : "Odd";
+}
 
 // Example Test Cases
 console.log(checkEvenOrOdd(4)); // "Even"
